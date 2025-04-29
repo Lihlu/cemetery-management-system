7d00e62495928f56528b3b0803a85dac49b560ca
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-29 14:18:22 +0200

    feat: create PublicUser CRUD endpoints

---
+++ cms-back-end/src/CemeteryManagementSystem.Application/Services/PublicUserService/Dto/PublicUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+
+namespace CemeteryManagementSystem.Services.PublicUserService.Dto
+{
+    public class PublicUserDto : EntityDto<Guid>
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address entered")]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [Phone]
+        [StringLength(10, ErrorMessage = "Please make sure contact number is 10 digits")]
+        public string ContactNo { get; set; }
+        [Required]
+        [StringLength(13, ErrorMessage = "Please make sure Identity Number is 13 digits")]
+        public string NationalIdNumber { get; set; }
+    }
+}
---
+++ cms-back-end/src/CemeteryManagementSystem.Application/Services/PublicUserService/PublicUserAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using CemeteryManagementSystem.Domain.PublicUser;
+using CemeteryManagementSystem.Services.PublicUserService.Dto;
+
+namespace CemeteryManagementSystem.Services.PublicUserService
+{
+    public class PublicUserAppService : AsyncCrudAppService<PublicUser, PublicUserDto, Guid>, IPublicUserAppService
+    {
+        public PublicUserAppService(IRepository<PublicUser, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
