0cafaac062225bf3ffa8c8a86023822dae166a35
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-29 14:17:42 +0200

    feat: create PublicUser

---
+++ cms-back-end/src/CemeteryManagementSystem.Core/Domain/PublicUser/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities.Auditing;
+
+namespace CemeteryManagementSystem.Domain.PublicUser
+{
+    public class Address : FullAuditedEntity<Guid>
+    {
+
+        public Guid PublicUserId { get; set; }
+        [ForeignKey(nameof(PublicUserId))]
+        public PublicUser PublicUser { get; set; }
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string ComplexOrEstateAddress { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ cms-back-end/src/CemeteryManagementSystem.Core/Domain/PublicUser/PublicUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities.Auditing;
+using CemeteryManagementSystem.Authorization.Users;
+
+namespace CemeteryManagementSystem.Domain.PublicUser
+{
+    public class PublicUser : FullAuditedEntity<Guid>
+    {
+        public string ContactNumber { get; set; }
+        public string NationalIdNumber { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+        public Address Address { get; set; }
+    }
+}
