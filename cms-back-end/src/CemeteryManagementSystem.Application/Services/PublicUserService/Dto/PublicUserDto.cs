f6edda6002c3b674f7ebc6b4b6e1c3f232d58842
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-29 16:16:43 +0200

    feat: add Dto

--- cms-back-end/src/CemeteryManagementSystem.Application/Services/PublicUserService/Dto/PublicUserDto.cs
+++ cms-back-end/src/CemeteryManagementSystem.Application/Services/PublicUserService/Dto/PublicUserDto.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using CemeteryManagementSystem.Domain.PublicUser;
 
 namespace CemeteryManagementSystem.Services.PublicUserService.Dto
 {
+    [AutoMap(typeof(PublicUser))]
     public class PublicUserDto : EntityDto<Guid>
     {
         [Required]
@@ -18,9 +21,10 @@ namespace CemeteryManagementSystem.Services.PublicUserService.Dto
         [Required]
         [Phone]
         [StringLength(10, ErrorMessage = "Please make sure contact number is 10 digits")]
-        public string ContactNo { get; set; }
+        public string ContactNumber { get; set; }
         [Required]
         [StringLength(13, ErrorMessage = "Please make sure Identity Number is 13 digits")]
         public string NationalIdNumber { get; set; }
+        public string Password { get; set; }
     }
 }
---
+++ cms-back-end/src/CemeteryManagementSystem.Application/Services/PublicUserService/Dto/UpdatePublicUserAddressDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Abp.AutoMapper;
+using CemeteryManagementSystem.Domain.PublicUser;
+
+namespace CemeteryManagementSystem.Services.PublicUserService.Dto
+{
+    [AutoMap(typeof(Address))]
+    public class UpdatePublicUserAddressDto
+    {
+        public Guid PublicUserId { get; set; }
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string ComplexOrEstateAddress { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
