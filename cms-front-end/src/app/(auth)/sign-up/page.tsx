cbb28964577a0fd5326f49a5f06e6dd341e627aa
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-30 00:07:27 +0200

    feat: add public user sign up

--- cms-front-end/src/app/(auth)/sign-up/page.tsx
+++ cms-front-end/src/app/(auth)/sign-up/page.tsx
@@ -1,20 +1,59 @@
 "use client";
-import React from "react";
-import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  LockOutlined,
+  MailOutlined,
+  UserOutlined,
+  PhoneOutlined,
+  IdcardOutlined,
+} from "@ant-design/icons";
+import { Button, Form, Input, Spin } from "antd";
 import { useStyles } from "./style/style";
 import PasswordInput from "@/components/auth-components/password-input/password-input";
+import { useRouter } from "next/navigation";
+import { toast } from "@/providers/toast/toast";
+import { ISignUpData } from "@/providers/auth/context";
+import { useAuthActions, useAuthState } from "@/providers/auth";
 
 const SignUp: React.FC = () => {
   const { styles } = useStyles();
+  const { isPending, isSuccess } = useAuthState();
+  const { signUp, resetStateFlags } = useAuthActions();
+  const router = useRouter();
+
+  const handleSignUp = async (values: ISignUpData) => {
+    try {
+      await signUp(values);
+    } catch (error) {
+      toast(error.message, "error");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast("Account created successfully! Please log in.", "success");
+      router.push("/login");
+
+      resetStateFlags();
+    }
+  }, [isSuccess, resetStateFlags, router]);
 
   return (
     <div className={styles.outerContainer}>
+      {isPending && (
+        <div className={styles.loadingOverlay}>
+          <Spin size="large" />
+        </div>
+      )}
       <div className={styles.formContainer}>
+        <h2 className={styles.formTitle}>Create an Account</h2>
         <Form
-          name="login"
+          name="signup"
           initialValues={{ remember: true }}
           style={{ maxWidth: 360 }}
+          onFinish={handleSignUp}
+          layout="vertical"
         >
           <Form.Item
             name="username"
@@ -22,30 +61,60 @@ const SignUp: React.FC = () => {
           >
             <Input prefix={<UserOutlined />} placeholder="Username" />
           </Form.Item>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please input your Name!" }]}
-          >
-            <Input prefix={<UserOutlined />} placeholder="Name" />
-          </Form.Item>
-          <Form.Item
-            name="surname"
-            rules={[{ required: true, message: "Please input your Surname!" }]}
-          >
-            <Input prefix={<UserOutlined />} placeholder="Surname" />
-          </Form.Item>
+
+          <div className={styles.nameFields}>
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: "Please input your Name!" }]}
+              className={styles.halfWidthInput}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Name" />
+            </Form.Item>
+
+            <Form.Item
+              name="surname"
+              rules={[
+                { required: true, message: "Please input your Surname!" },
+              ]}
+              className={styles.halfWidthInput}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Surname" />
+            </Form.Item>
+          </div>
+
           <Form.Item
             name="email"
-            rules={[{ required: true, message: "Please input your Email" }]}
+            rules={[
+              { required: true, message: "Please input your Email" },
+              { type: "email", message: "Please enter a valid email address" },
+            ]}
           >
             <Input prefix={<MailOutlined />} type="email" placeholder="Email" />
           </Form.Item>
+
+          <Form.Item
+            name="contactNumber"
+            rules={[
+              { required: true, message: "Please input your contact number" },
+            ]}
+          >
+            <Input prefix={<PhoneOutlined />} placeholder="Contact Number" />
+          </Form.Item>
+
+          <Form.Item
+            name="nationalIdNumber"
+            rules={[{ required: true, message: "Please input your ID Number" }]}
+          >
+            <Input prefix={<IdcardOutlined />} placeholder="ID Number" />
+          </Form.Item>
+
           <Form.Item
             name="password"
             rules={[{ required: true, message: "Please input your Password!" }]}
           >
             <PasswordInput />
           </Form.Item>
+
           <Form.Item
             name="confirmPassword"
             dependencies={["password"]}
@@ -69,10 +138,14 @@ const SignUp: React.FC = () => {
               placeholder="Confirm Password"
             />
           </Form.Item>
+
           <Form.Item>
-            <Button block type="primary" htmlType="submit">
+            <Button block type="primary" htmlType="submit" disabled={isPending}>
               Sign Up
             </Button>
+            <div className={styles.loginLink}>
+              Already have an account? <a href="/login">Log in</a>
+            </div>
           </Form.Item>
         </Form>
       </div>
