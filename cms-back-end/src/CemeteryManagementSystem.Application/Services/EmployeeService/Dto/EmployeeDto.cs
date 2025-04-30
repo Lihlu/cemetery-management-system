06fa1d05910c19dddc45578ec60f891326f33996
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-30 16:26:02 +0200

    feat: create employee app service

---
+++ cms-back-end/src/CemeteryManagementSystem.Application/Services/EmployeeService/Dto/EmployeeDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using CemeteryManagementSystem.Domain.Employee;
+
+namespace CemeteryManagementSystem.Services.EmployeeService.Dto
+{
+    [AutoMap(typeof(Employee))]
+    public class EmployeeDto : EntityDto<Guid>
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address entered")]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string EmployeeNumber { get; set; }
+        [Required]
+        public string Section { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ cms-back-end/src/CemeteryManagementSystem.Application/Services/EmployeeService/IEmployeeAppService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Abp.Application.Services;
+using CemeteryManagementSystem.Services.EmployeeService.Dto;
+
+namespace CemeteryManagementSystem.Services.EmployeeService
+{
+    public interface IEmployeeAppService : IAsyncCrudAppService<EmployeeDto, Guid>
+    {
+    }
+}
