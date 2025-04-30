3759a9985f2c5d0e06e79aaa22c6b304ea91a166
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-30 16:25:02 +0200

    feat: create employee entity and manager

---
+++ cms-back-end/src/CemeteryManagementSystem.Core/Domain/Employee/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using CemeteryManagementSystem.Authorization.Users;
+
+namespace CemeteryManagementSystem.Domain.Employee
+{
+    public class Employee : FullAuditedEntity<Guid>
+    {
+        public string EmployeeNumber { get; set; }
+        public string Section { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+    }
+}
---
+++ cms-back-end/src/CemeteryManagementSystem.Core/Domain/Employee/EmployeeManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Abp.UI;
+using CemeteryManagementSystem.Authorization.Users;
+
+namespace CemeteryManagementSystem.Domain.Employee
+{
+    public class EmployeeManager : DomainService
+    {
+        private readonly UserManager _userManager;
+        private readonly IRepository<Employee, Guid> _employeeRepository;
+
+        public EmployeeManager(UserManager userManager, IRepository<Employee, Guid> employeeRepository)
+        {
+            _userManager = userManager;
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<Employee> CreateEmployeeAsync(string username, string firstName, string surname, string emailAddress, string password, string employeeNumber, string section)
+        {
+            // Check if the username is already taken
+            var existingUser = await _userManager.FindByNameAsync(username);
+            if (existingUser != null)
+            {
+                throw new UserFriendlyException($"Username '{username}' is already taken.");
+            }
+
+            // Check if the email is already used
+            var existingEmailUser = await _userManager.FindByEmailAsync(emailAddress);
+            if (existingEmailUser != null)
+            {
+                throw new UserFriendlyException($"Email '{emailAddress}' is already in use.");
+            }
+
+            var user = new User
+            {
+                Name = firstName,
+                Surname = surname,
+                EmailAddress = emailAddress,
+                UserName = username,
+            };
+
+            var userCreationResult = await _userManager.CreateAsync(user, password);
+            if (!userCreationResult.Succeeded)
+            {
+                throw new UserFriendlyException($"User creation failed");
+            }
+
+            await _userManager.AddToRoleAsync(user, "employee");
+
+            Employee employee = new Employee
+            {
+                EmployeeNumber = employeeNumber,
+                Section = section,
+                User = user,
+            };
+
+            var result = await _employeeRepository.InsertAsync(employee);
+
+            return result;
+
+        }
+    }
+}
