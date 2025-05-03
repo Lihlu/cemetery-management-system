7930066b2ecc6c132388febebaa719114a8ed2f4
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-05-03 17:10:23 +0200

    feat: create BottomNav component

---
+++ cms-front-end/src/components/shared/bottom-nav/bottom-nav.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { Button, Space } from "antd";
+import { usePathname, useRouter } from "next/navigation";
+import { useStyles } from "./style/style";
+
+interface NavItem {
+  key: string;
+  label: string;
+  icon: React.ReactNode;
+}
+
+interface BottomNavProps {
+  navItems: NavItem[];
+}
+
+const BottomNav = ({ navItems }: BottomNavProps) => {
+  const { styles } = useStyles();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleNavClick = (key: string) => {
+    router.push(key);
+  };
+
+  return (
+    <div className={styles.bottomNav}>
+      <Space align="center" size="large">
+        {navItems.map((item) => {
+          const isActive = pathname.startsWith(item.key);
+          return (
+            <Button
+              key={item.key}
+              type="text"
+              icon={item.icon}
+              className={`${styles.bottomNavButton} ${isActive ? styles.bottomNavButtonActive : ""}`}
+              onClick={() => handleNavClick(item.key)}
+              aria-label={item.label}
+              style={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <div className={styles.bottomNavLabel}>{item.label}</div>
+            </Button>
+          );
+        })}
+      </Space>
+    </div>
+  );
+};
+
+export default BottomNav;
