7ad7e0c968ee66ff28be9094d132e3feb5964f09
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-05-08 00:05:38 +0200

    feat: fetch bookings by user Id

---
+++ cms-back-end/src/CemeteryManagementSystem.Core/Domain/Bookings/BookingManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace CemeteryManagementSystem.Domain.Bookings
+{
+    public class BookingManager : DomainService
+    {
+        private readonly IRepository<Booking, Guid> _bookingRepository;
+        public BookingManager(IRepository<Booking, Guid> bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        public async Task<IEnumerable<Booking>> GetBookingsByUserId(long userId)
+        {
+           var bookings = await _bookingRepository.GetAll()
+                .Include(b => b.DeceasedPerson)
+                .Where(b => b.BookerId == userId)
+                .ToListAsync();
+
+            return bookings;
+        }
+    }
+}
