72a0f41b7543826aa3d78214d656ae03157f4f4f
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-05-07 03:58:53 +0200

    style: updated login page styling

--- cms-front-end/src/app/(auth)/login/page.tsx
+++ cms-front-end/src/app/(auth)/login/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 import React, { useEffect } from "react";
-import { UserOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Spin } from "antd";
+import { UserOutlined, LockOutlined, LoginOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Spin, Typography, Divider, Card } from "antd";
 import { useStyles } from "./style/style";
-import PasswordInput from "@/components/auth-components/password-input/password-input";
 import { ILoginData } from "@/providers/auth/context";
 import { useAuthActions, useAuthState } from "@/providers/auth";
 import { useRouter } from "next/navigation";
 import { toast } from "@/providers/toast/toast";
+import Link from "next/link";
+
+const { Title, Text } = Typography;
 
 const LoginPage: React.FC = () => {
   const { styles } = useStyles();
   const { loginUser, resetStateFlags } = useAuthActions();
   const { isSuccess, isError, isPending, currentRole } = useAuthState();
   const router = useRouter();
+  const [form] = Form.useForm();
 
   const handleSignIn = async (values: ILoginData) => {
     try {
@@ -53,50 +56,95 @@ const LoginPage: React.FC = () => {
   }, [isSuccess, isError, currentRole, resetStateFlags, router]);
 
   return (
-    <div className={styles.outerContainer}>
+    <div className={styles.pageContainer}>
       {isPending && (
         <div className={styles.loadingOverlay}>
           <Spin size="large" />
         </div>
       )}
-      <div className={styles.formContainer}>
-        <Form
-          name="login"
-          initialValues={{ remember: true }}
-          style={{ maxWidth: 360 }}
-          onFinish={handleSignIn}
-        >
-          <Form.Item
-            name="userNameOrEmailAddress"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Username or Email!",
-              },
-            ]}
-          >
-            <Input
-              prefix={<UserOutlined />}
-              placeholder="Username or Email Address"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
-          >
-            <PasswordInput />
-          </Form.Item>
-          <Form.Item>
-            <a href="/forgot-password">Forgot password</a>
-          </Form.Item>
+      <div className={styles.contentWrapper}>
+        <Card bordered={false} className={styles.loginCard}>
+          <div className={styles.logoContainer}>
+            <div className={styles.logo}>
+              <img src="/images/hero-image.png" alt="Logo" width="120px" />
+               
+            </div>
+          </div>
+          <Title level={2} className={styles.formTitle}>Welcome Back</Title>
+          <Text type="secondary" className={styles.formSubtitle}>
+            Sign in to continue to your account
+          </Text>
 
-          <Form.Item>
-            <Button block type="primary" htmlType="submit" disabled={isPending}>
-              Log in
-            </Button>
-            or <a href="/sign-up">Register now!</a>
-          </Form.Item>
-        </Form>
+          <Form
+            form={form}
+            name="login"
+            initialValues={{ remember: true }}
+            className={styles.loginForm}
+            onFinish={handleSignIn}
+            layout="vertical"
+          >
+            <Form.Item
+              name="userNameOrEmailAddress"
+              label="Username or Email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your username or email",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className={styles.inputIcon} />}
+                placeholder="Enter your username or email"
+                className={styles.input}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                { required: true, message: "Please enter your password" }
+              ]}
+            >
+              <Input.Password
+                placeholder="Enter your password" 
+                prefix={<LockOutlined className={styles.inputIcon} />}
+                className={styles.input}
+              />
+            </Form.Item>
+
+            <div className={styles.forgotPasswordContainer}>
+              <Link href="/forgot-password" className={styles.forgotPassword}>
+                Forgot password?
+              </Link>
+            </div>
+
+            <Form.Item className={styles.submitContainer}>
+              <Button 
+                block 
+                type="primary" 
+                htmlType="submit" 
+                disabled={isPending}
+                className={styles.submitButton}
+              >
+                Sign In
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <Divider className={styles.divider}>
+            <Text type="secondary">New to our platform?</Text>
+          </Divider>
+
+          <div className={styles.registerContainer}>
+            <Link href="/sign-up">
+              <Button block size="large" className={styles.registerButton}>
+                Create an Account
+              </Button>
+            </Link>
+          </div>
+        </Card>
       </div>
     </div>
   );
