646c78e2cdd7cbb037c41ff25746bd477521f098
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-05-09 02:10:19 +0200

    feat: add get by owner id endpoint

---
+++ cms-back-end/src/CemeteryManagementSystem.Core/Domain/GraveSite/GravesiteManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using CemeteryManagementSystem.Domain.Bookings;
+using Microsoft.EntityFrameworkCore;
+
+namespace CemeteryManagementSystem.Domain.GraveSite
+{
+    public class GravesiteManager : DomainService
+    {
+        private readonly IRepository<GraveSite, Guid> _graveSiteRepository;
+        public GravesiteManager(IRepository<GraveSite, Guid> graveSiteRepository)
+        {
+            _graveSiteRepository = graveSiteRepository;
+        }
+
+        public async Task<IEnumerable<GraveSite>> GetGraveSitesByOwnerId(long ownerId)
+        {
+            return await _graveSiteRepository.GetAll()
+                 .Where(g => g.OwnerId == ownerId)
+                 .ToListAsync();
+        }
+    }
+}
