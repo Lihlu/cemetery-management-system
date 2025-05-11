deaf9706f6d85f0b5bfc35821985cfb748f40805
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-05-11 23:16:28 +0200

    feat: update database

---
+++ cms-back-end/src/CemeteryManagementSystem.EntityFrameworkCore/Migrations/20250511195237_AddRowAndCol.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CemeteryManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowAndCol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Column",
+                table: "GraveSites",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Row",
+                table: "GraveSites",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<List<int>>(
+                name: "sitesPerRow",
+                table: "CemeterySections",
+                type: "integer[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Column",
+                table: "GraveSites");
+
+            migrationBuilder.DropColumn(
+                name: "Row",
+                table: "GraveSites");
+
+            migrationBuilder.DropColumn(
+                name: "sitesPerRow",
+                table: "CemeterySections");
+        }
+    }
+}
