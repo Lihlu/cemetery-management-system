d5a2cb9d399ef7f044ace88c6c1d0cea0e7bd63b
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-05-12 16:58:14 +0200

    feat: update formatting

--- cms-front-end/src/app/employee/page.tsx
+++ cms-front-end/src/app/employee/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const EmployeePage = () => {
-    return (
-        <div>
-        <h1>Employee Page</h1>
-        <p>This is the employee page content.</p>
-        </div>
-    );
-    };
+  return (
+    <div>
+      <h1>Employee Page</h1>
+      <p>This is the employee page content.</p>
+    </div>
+  );
+};
 
 export default EmployeePage;
